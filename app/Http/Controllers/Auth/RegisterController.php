4f85856e1bdd662378d3cf030c1cd1292e105190
Ornel Stanley <ornelstanley@gmail.com>
2021-03-01 10:31:01 +0100

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -55,8 +55,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'username' => ['required', 'string', 'min:5'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
             'country' => ['required'],
             'phone_no' => ['required'],
             'traderID' => ['required']
