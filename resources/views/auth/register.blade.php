3ef2cfcc4c7babedc68aa5e0353d80ec3bc282bf
Ornel Stanley <ornelstanley@gmail.com>
2021-03-03 23:03:20 +0100

    update register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -346,7 +346,6 @@
 													<!---->
 													
 													<!---->
-													<i tabindex="-2" class="mdc-text-field__icon error-icon material-icons am-typography--color-inherit" role="button">visibility_off</i>
 													<!---->
 												</div>
 												<!---->
